393a5f4fcc4c340709ea619d076a66b58c8129e3
Ryan Ebbers <info@ryanebbers.com>
2015-11-17 15:32:54 -0500

    added tooltips to the sidenav

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -7,6 +7,10 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
 
     <script>
+        $(function () {
+            $('[data-toggle="tooltip"]').tooltip()
+        })
+
         // DOCUMENT READY
         $(document).ready(function() {
             $('.wrapper').animate({
--- resources/views/includes/sideNav.blade.php
+++ resources/views/includes/sideNav.blade.php
@@ -1,11 +1,11 @@
 <div class="left_top_nav">
-    <a href="{{ url() }}">
+    <a href="{{ url() }}" data-toggle="tooltip" data-placement="right" title="Home">
         <i class="fa fa-2x fa-home"></i>
     </a>
-    <a href="{{ url('contact') }}">
+    <a href="{{ url('contact') }}" data-toggle="tooltip" data-placement="right" title="Contact Me">
         <i class="fa fa-2x fa-phone"></i>
     </a>
-    <a href="{{ url('about') }}">
+    <a href="{{ url('about') }}" data-toggle="tooltip" data-placement="right" title="About Me">
         <i class="fa fa-2x fa-user"></i>
     </a>
 </div>
