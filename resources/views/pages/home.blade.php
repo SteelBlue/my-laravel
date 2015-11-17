50bb7e136a13568a3cdb83294e1555cc12f80693
Ryan Ebbers <info@ryanebbers.com>
2015-11-17 14:36:57 -0500

    updated homepage text

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,12 +2,12 @@
 
 @section('content')
 
-    <div class="wrapper">
+    <div id="home" class="wrapper">
         <div class="container">
             <div class="content">
                 <div class="title">
                     <h1>Ryan Ebbers</h1>
-                    <h2>DEVELOPED WITH LARAVEL 5</h2>
+                    <h2>DEVELOPED USING LARAVEL 5</h2>
                 </div>
             </div>
         </div>
