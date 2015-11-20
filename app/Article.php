3e11bb1665e9a9cc5c802742e4fa512d0043752a
Ryan Ebbers <info@ryanebbers.com>
2015-11-20 15:13:43 -0500

    added fillable content to the Articles

--- app/Article.php
+++ app/Article.php
@@ -6,7 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    // Fillable Fields
+    /**
+     * Fillable Fields
+     */
     protected $fillable = [
         'title',
         'body',
