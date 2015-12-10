daf52858d6bc1310ba420873c41d360fb551449f
Ryan Ebbers <info@ryanebbers.com>
2015-12-10 16:35:06 -0500

    created relationship between an article and the user who created it

--- app/Article.php
+++ app/Article.php
@@ -60,4 +60,14 @@ class Article extends Model
             $this->attributes['published_at'] = Carbon::parse($date);
         endif;
     }
+
+    /**
+     * An article is owned by a user.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo('User');
+    }
 }
