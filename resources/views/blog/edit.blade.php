ef4983730ddd023f011756ee26d8da29eb001072
Ryan Ebbers <info@ryanebbers.com>
2015-12-10 14:54:52 -0500

    created edit form, used eloquent form model binding to fill the form

--- resources/views/blog/edit.blade.php
+++ resources/views/blog/edit.blade.php
@@ -3,8 +3,44 @@
 @section('content')
 
     <div class="title">
-        <h1>Edit: This Article</h1>
+        <h1>Edit</h1>
+        <h2>{{ $article->title }}</h2>
         <hr>
     </div>
+
+    {{-- Display Form Errors --}}
+    @if ($errors->any())
+        <ul class="alert alert-danger">
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
+    {{-- end form errors --}}
+
+    {{-- Create Article Form --}}
+    {!! Form::model($article, ['method' => 'PATCH', 'action' => ['ArticlesController@update', $article->id]]) !!}
+
+        <div class="form-group">
+            {!! Form::label('title', 'Title') !!}
+            {!! Form::text('title', null, ['class' => 'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('body', 'Body') !!}
+            {!! Form::textarea('body', null, ['class' => 'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('published_at', 'Publish On') !!}
+            {!! Form::input('date', 'published_at', date('Y-m-d'), ['class' => 'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::submit('Update Article', ['class' => 'btn btn-primary form-control']) !!}
+        </div>
+
+    {!! Form::close() !!}
+    {{-- end article form --}}
 
 @stop
