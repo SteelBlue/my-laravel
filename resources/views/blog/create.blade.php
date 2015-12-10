4a234ee058f89a77dd8d658b9987f3e2c2b84e81
Ryan Ebbers <info@ryanebbers.com>
2015-12-10 15:43:25 -0500

    created a partial which passes a variable with the submit button text for the article form

--- resources/views/blog/create.blade.php
+++ resources/views/blog/create.blade.php
@@ -12,7 +12,6 @@
     @if (Session::has('flash_notification.message'))
         <div class="alert alert-{{ Session::get('flash_notification.level') }}">
             <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-
             {{ Session::get('flash_notification.message') }}
         </div>
     @endif
@@ -24,26 +23,7 @@
 
     {{-- Create Article Form --}}
     {!! Form::open(['action' => 'ArticlesController@store']) !!}
-
-        <div class="form-group">
-            {!! Form::label('title', 'Title') !!}
-            {!! Form::text('title', null, ['class' => 'form-control']) !!}
-        </div>
-
-        <div class="form-group">
-            {!! Form::label('body', 'Body') !!}
-            {!! Form::textarea('body', null, ['class' => 'form-control']) !!}
-        </div>
-
-        <div class="form-group">
-            {!! Form::label('published_at', 'Publish On') !!}
-            {!! Form::input('date', 'published_at', date('Y-m-d'), ['class' => 'form-control']) !!}
-        </div>
-
-        <div class="form-group">
-            {!! Form::submit('Add Article', ['class' => 'btn btn-primary form-control']) !!}
-        </div>
-
+        @include('blog._form', ['submitButtonText' => 'Add Article'])
     {!! Form::close() !!}
     {{-- end article form --}}
 
--- resources/views/blog/edit.blade.php
+++ resources/views/blog/edit.blade.php
@@ -14,26 +14,7 @@
 
     {{-- Create Article Form --}}
     {!! Form::model($article, ['method' => 'PATCH', 'action' => ['ArticlesController@update', $article->id]]) !!}
-
-        <div class="form-group">
-            {!! Form::label('title', 'Title') !!}
-            {!! Form::text('title', null, ['class' => 'form-control']) !!}
-        </div>
-
-        <div class="form-group">
-            {!! Form::label('body', 'Body') !!}
-            {!! Form::textarea('body', null, ['class' => 'form-control']) !!}
-        </div>
-
-        <div class="form-group">
-            {!! Form::label('published_at', 'Publish On') !!}
-            {!! Form::input('date', 'published_at', date('Y-m-d'), ['class' => 'form-control']) !!}
-        </div>
-
-        <div class="form-group">
-            {!! Form::submit('Update Article', ['class' => 'btn btn-primary form-control']) !!}
-        </div>
-
+        @include('blog._form', ['submitButtonText' => 'Update Article'])
     {!! Form::close() !!}
     {{-- end article form --}}
 
