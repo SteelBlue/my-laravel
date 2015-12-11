1b370323ceef01b3c4c8000ea6885f0f235644bb
Ryan Ebbers <info@ryanebbers.com>
2015-12-11 15:30:25 -0500

    added content to experience page

--- resources/views/pages/experience.blade.php
+++ resources/views/pages/experience.blade.php
@@ -6,8 +6,28 @@
         <h1>My Experiences</h1>
     </div>
     <div class="copy">
-        <h2>Developus Maximus</h2>
-        <p>This will become text about the legend of Ryan Ebbers.</p>
+        <ul class="list-unstyled">
+            <li>
+                <h2>Dunn&Co.</h2>
+                <h3>2014 - NOW</h3>
+                <h4>Digital Developer</h4>
+                <p>I currently work here. It's awesome!</p>
+            </li>
+            <hr>
+            <li>
+                <h2>BarProducts.com</h2>
+                <h3>2012 - 2013</h3>
+                <h4>Web Developer</h4>
+                <p>I worked here for a bit. This is where I had a chance to cut my teeth.</p>
+            </li>
+            <hr>
+            <li>
+                <h2>Pepper Contracting</h2>
+                <h3>2009 - 2011</h3>
+                <h4>Project Engineer</h4>
+                <p>Worked in many different areas of this company, and learned the responsibilty of many different positions.</p>
+            </li>
+        </ul>
     </div>
 
 @endsection
