a4ff0cf6011ac32befdcf057ca04ea87ef346f22
Ryan Ebbers <info@ryanebbers.com>
2015-12-15 14:56:25 -0500

    added edit-article link on the show article template

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -4,15 +4,19 @@
 
     <div class="title">
         <h1>Blog</h1>
-        <h2>DEVELOPED USING LARAVEL 5</h2>
+        <h2>
+            <a href="#">{{ $article->title }}</a>
+        </h2>
         <hr>
+        @if (Auth::check())
+            <h3>
+                <a href="{{ action('ArticlesController@edit', $article->id) }}">Edit Article</a>
+            </h3>
+            <hr>
+        @endif
     </div>
 
     <article>
-
-        <h3>
-            <a href="#">{{ $article->title }}</a>
-        </h3>
 
         <div class="body">
             {{ $article->body }}
