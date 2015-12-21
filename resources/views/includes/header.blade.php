845d2c98634ad0409ae6d758ea3ba941e0360ae7
Ryan Ebbers <info@ryanebbers.com>
2015-12-21 16:19:31 -0500

    added cache busting to the css files

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ url() }}/css/app.css" type="text/css">
+    <link rel="stylesheet" href="{{ elixir('css/all.css') }}" type="text/css">
 </head>
 <body>
 
