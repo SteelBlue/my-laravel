49bf186482e6cb0dbc753634a1aa6847dee46a4c
Ryan Ebbers <info@ryanebbers.com>
2015-12-21 16:55:10 -0500

    changed where user gets redirected after login

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -23,7 +23,7 @@ class AuthController extends Controller
 
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
 
-    protected $redirectTo = '/blog';
+    protected $redirectTo = '/';
 
     /**
      * Create a new authentication controller instance.
