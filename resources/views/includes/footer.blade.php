7766ee6b5363ffa05f9051537254ed06d574af6f
Ryan Ebbers <info@ryanebbers.com>
2015-12-24 11:23:24 -0500

    added the select2 plugin, setup the tag_list to use select2, made the select2-container display:block

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -2,10 +2,13 @@
         </div>
     </div>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="//code.jquery.com/jquery.js"></script>
     <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.1/js/select2.min.js"></script>
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
+
+    @yield('footer')
 
     <script>
         $('div.alert').not('.alert-important').delay(3000).slideUp();
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -3,10 +3,11 @@
 <head>
     <title>Ryan Ebbers | Web Developer, App Developer, Entrepreneur</title>
 
-    <link href='https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900' rel='stylesheet' type='text/css'>
+    <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900' type='text/css'>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.1/css/select2.min.css" />
     <link rel="stylesheet" href="{{ elixir('css/all.css') }}" type="text/css">
 </head>
 <body>
