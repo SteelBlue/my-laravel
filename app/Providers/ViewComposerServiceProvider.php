936c3978261858a9886d57698d46e341aae39fc7
Ryan Ebbers <info@ryanebbers.com>
2016-01-04 17:08:57 -0500

    extracted to a method, added doc blocks

--- app/Providers/ViewComposerServiceProvider.php
+++ app/Providers/ViewComposerServiceProvider.php
@@ -14,10 +14,7 @@ class ViewComposerServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        view()->composer('partials.sideNav', function($view)
-        {
-            $view->with('latest', Article::latest()->first());
-        });
+        $this->composeNavigation();
     }
 
     /**
@@ -28,5 +25,15 @@ class ViewComposerServiceProvider extends ServiceProvider
     public function register()
     {
         //
+    }
+
+    /**
+     * Compose the navigation view, with the $latest variable.
+     */
+    private function composeNavigation()
+    {
+        view()->composer('partials.sideNav', function ($view) {
+            $view->with('latest', Article::latest()->first());
+        });
     }
 }
